ab2f819fcbc8cb103be45cb155e3527464fd2347
Wathiqa Ahmed <38113360+watheqahahmed@users.noreply.github.com>
2025-09-09 21:33:41 +0300

    feat: Add missing contact() method to ShopController

--- app/Http/Controllers/ShopController.php
+++ app/Http/Controllers/ShopController.php
@@ -6,10 +6,35 @@ use Illuminate\Http\Request;
 
 class ShopController extends Controller
 {
-    // دالة عرض الصفحة الترحيبية
+    /**
+     * Show the home page.
+     */
     public function index()
     {
-        // ترجع العرض من resources/views/shop/welcome.blade.php
         return view('shop.index');
+    }
+
+    /**
+     * Show the about page.
+     */
+    public function about()
+    {
+        return view('shop.about');
+    }
+
+    /**
+     * Show the contact page.
+     */
+    public function contact()
+    {
+        return view('shop.contact');
+    }
+
+    /**
+     * Show the products page.
+     */
+    public function products()
+    {
+        return view('shop.products');
     }
 }
